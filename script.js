47c12a1e23ef4e52272d111d8caddf2ef8a5d596
abhay0933 <abhay80912@gmail.com>
2023-10-24 18:52:58 +0530

    images changed

---
+++ script.js
@@ -0,0 +1,57 @@
+let order_section = document.querySelector(".order");
+let orderid = 432;
+
+const order_btn = document.querySelector(".order_btn");
+order_btn.addEventListener("click", () => {
+  let burger = document.getElementById("burger");
+  let fries = document.getElementById("Fries");
+  let coldDrink = document.getElementById("cold-Drink");
+
+  let orderID = document.querySelector(".order_id");
+  orderID.innerText = "order ID: " + orderid++;
+
+  let new_order = document.createElement("div");
+
+  order_section.innerHTML = "";
+
+  let loader = document.querySelector(".loading");
+  loader.classList.add("loader");
+  let promise1 = new Promise((resolve, reject) => {
+    if (burger.checked == true) {
+      resolve("burger");
+    }
+    
+  });
+  let promise2 = new Promise((resolve, reject) => {
+    if (fries.checked == true) {
+      resolve("fries");
+    }
+  });
+  let promise3 = new Promise((resolve, reject) => {
+    if (coldDrink.checked == true) {
+      resolve("cold drink");
+    }
+  });
+
+  setTimeout(() => {
+    promise1.then(value => {
+      let image = document.createElement("img");
+      image.setAttribute("src", value + ".jpg");
+      new_order.appendChild(image);
+    });
+    promise2.then(value => {
+      let image = document.createElement("img");
+      image.setAttribute("src", value + ".jpg");
+      new_order.appendChild(image);
+    });
+    promise3.then(value => {
+      let image = document.createElement("img");
+      image.setAttribute("src", value + ".jpg");
+      new_order.appendChild(image);
+    });
+    setTimeout(() => {
+      loader.classList.remove("loader");
+      order_section.innerHTML = new_order.innerHTML;
+    }, 2000);
+  }, 2000);
+});
